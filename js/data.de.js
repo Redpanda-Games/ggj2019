d1e161df27599ce8414606bc61da9f60c6f515ba
Gummibeer <dev.gummibeer@gmail.com>
2019-01-26 21:10:57 +0100

    add new levels

--- js/data.de.js
+++ js/data.de.js
@@ -28,12 +28,126 @@ window.states.de = {
                 button: "Lege eine Spur mit Schneckennahrung, die vom Pilz weg führt",
                 success: true,
                 message: "Gemeinsam schaut ihr der klebrigen Schnecke zu, wie sie davon kriecht. Das sieht ziemlich lustig aus. Der Pilz hilft Dir weiter.",
-                next: true,
+                next: 30,
             },
             2: {
                 id: 2,
                 button: "Streu etwas Salz auf die Schnecke",
                 success: false,
+            },
+        }
+    },
+    30: {
+        situation: "Du siehst einen Schnabelpilz mit kräftigem Kiefer.",
+        gameOver: "Er beißt Dir den Stiel ab und Du verendest an Ort und Stelle.",
+        image: "img/3-bitey.png",
+        actions: {
+            1: {
+                id: 1,
+                button: "Biete ihm eine Walnuss an",
+                success: true,
+                message: "Er schnappt nach der Walnuss und isst sie genüsslich mitsamt der Schale. “Die knacken so schön!” Gern zeigt er Dir den Weg.",
+                next: 40,
+            },
+            2: {
+                id: 2,
+                button: "Stupse ihn an",
+                success: false,
+            },
+        }
+    },
+    40: {
+        situation: "Du siehst die hübsche Babsi, sie zwinkert Dir zu.",
+        gameOver: "Das hättest Du nicht tun sollen. Sie fühlt sich gekränkt und ignoriert Dich.",
+        image: "img/4-boobies.png",
+        actions: {
+            1: {
+                id: 1,
+                button: "Mach ihr ein Kompliment",
+                success: true,
+                message: "Sie macht einen weiteren ihrer verführerischen Augenaufschläge. Diesmal in die Richtung Deiner heimischen Lichtung.",
+                next: 50,
+            },
+            2: {
+                id: 2,
+                button: "Pflücke eine ihrer Vorder-Blumen",
+                success: false,
+            },
+        }
+    },
+    50: {
+        situation: "Du siehst die eindrucksvolle Becky.",
+        gameOver: "Sie findet ihn nicht lustig und lässt Dich einfach stehen.",
+        image: "img/5-glatze.png",
+        actions: {
+            1: {
+                id: 1,
+                button: "Reiß einen Witz über Glatzen",
+                success: false,
+            },
+            2: {
+                id: 2,
+                button: "Gib ihr Deinen unbenutzten Lippenstift",
+                success: true,
+                message: "Sie frischt ihr Lippenrot auf und plaudert mit Dir über Dein Heim.",
+                next: 60,
+            },
+        }
+    },
+    60: {
+        situation: "Dieser erfahrene Veteran ist in Ehre gealtert. Er schaut Dich misstrauisch an.",
+        gameOver: "Er schnauft nicht mal und lässt Dich links liegen.",
+        image: "img/6-old-grandpa.png",
+        actions: {
+            1: {
+                id: 1,
+                button: "Erringe sein Vertrauen mit einem schmutzigen Witz, den Du in Deiner Lichtung aufgeschnappt hast",
+                success: true,
+                message: "Er lässt sich zu einem Schmunzeln herab und erinnert sich an seinen alten Freund Herbert. Er erkundigt sich, wie es ihm geht und erklärt detailreich.",
+                next: 70,
+            },
+            2: {
+                id: 2,
+                button: "Erzähl ihm einen Kinderwitz",
+                success: false,
+            },
+        }
+    },
+    70: {
+        situation: "Diese Dame ist ein Blickfang. Sie hat sich mit ein paar Farnen geschmückt.",
+        gameOver: "Sie dreht sich weg und Du bist auf Dich allein gestellt.",
+        image: "img/7-lady.png",
+        actions: {
+            1: {
+                id: 1,
+                button: "Frag sie nach dem Weg",
+                success: true,
+                message: "Sie lässt sich herab und säuselt Dir die Richtung der Heimat ins Ohr.",
+                next: 80,
+            },
+            2: {
+                id: 2,
+                button: "Versuch ihr unter den Schirm zu schauen",
+                success: false,
+            },
+        }
+    },
+    80: {
+        situation: "Dieser kleine Freund ist besonders fleißig. Außerdem spielt er gern.",
+        gameOver: "Er zermatscht und Du bist klebrig und allein.",
+        image: "img/8-mario.png",
+        actions: {
+            1: {
+                id: 1,
+                button: "Spring auf ihn",
+                success: false,
+            },
+            2: {
+                id: 2,
+                button: "Erzähl ihm von Deinem Lieblings Gameboy Spiel",
+                success: true,
+                message: "Er kennt einige der Spiele und wundert sich über gewisse Ähnlichkeiten. Den Weg zu Deiner Lichtung sagt er Dir gern.",
+                next: true,
             },
         }
     },
--- js/data.en.js
+++ js/data.en.js
@@ -28,12 +28,126 @@ window.states.en = {
                 button: "Lay a trail with snail food that leads away from the mushroom",
                 success: true,
                 message: "Together you watch the sticky snail crawl away. That looks pretty funny. The mushroom helps you.",
-                next: true,
+                next: 30,
             },
             2: {
                 id: 2,
                 button: "Sprinkle some salt on the snail",
                 success: false,
+            },
+        }
+    },
+    30: {
+        situation: "You see a beaked mushroom with a strong jaw.",
+        gameOver: "He bites off your handle and you die on the spot.",
+        image: "img/3-bitey.png",
+        actions: {
+            1: {
+                id: 1,
+                button: "Offer him a walnut",
+                success: true,
+                message: "He snaps at the walnut and eats it with relish along with its shell. \"They crack so beautifully!\" He'll gladly show you the way.",
+                next: 40,
+            },
+            2: {
+                id: 2,
+                button: "Poke him",
+                success: false,
+            },
+        }
+    },
+    40: {
+        situation: "You see that pretty Babsi, she's winking at you.",
+        gameOver: "You shouldn't have done that. She feels offended and ignores you.",
+        image: "img/4-boobies.png",
+        actions: {
+            1: {
+                id: 1,
+                button: "Give her a compliment",
+                success: true,
+                message: "She makes another one of her seductive blindfolds. This time in the direction of your home clearing.",
+                next: 50,
+            },
+            2: {
+                id: 2,
+                button: "Pick one of her front flowers",
+                success: false,
+            },
+        }
+    },
+    50: {
+        situation: "You see the impressive Becky.",
+        gameOver: "She doesn't find it funny and just leaves you standing.",
+        image: "img/5-glatze.png",
+        actions: {
+            1: {
+                id: 1,
+                button: "Make a joke about baldness",
+                success: false,
+            },
+            2: {
+                id: 2,
+                button: "Give her your unused lipstick",
+                success: true,
+                message: "She refreshes her lip red and chats with you about your home.",
+                next: 60,
+            },
+        }
+    },
+    60: {
+        situation: "This experienced veteran has aged in honor. He looks at you suspiciously.",
+        gameOver: "He doesn't even breathe and lets you down.",
+        image: "img/6-old-grandpa.png",
+        actions: {
+            1: {
+                id: 1,
+                button: "Earn his trust with a dirty joke you picked up in your clearing",
+                success: true,
+                message: "He lets himself down to a smile and remembers his old friend Herbert. He inquires how he is doing and explains in detail.",
+                next: 70,
+            },
+            2: {
+                id: 2,
+                button: "Tell him a kid joke",
+                success: false,
+            },
+        }
+    },
+    70: {
+        situation: "This lady is an eye-catcher. She has adorned herself with a few ferns.",
+        gameOver: "She turns away and you're on your own.",
+        image: "img/7-lady.png",
+        actions: {
+            1: {
+                id: 1,
+                button: "Ask her the way",
+                success: true,
+                message: "She lets herself down and purrs the direction of her homeland into your ear.",
+                next: 80,
+            },
+            2: {
+                id: 2,
+                button: "Try to look under her umbrella",
+                success: false,
+            },
+        }
+    },
+    80: {
+        situation: "This little friend is especially hard-working. He also likes to play.",
+        gameOver: "He crushes and you're sticky and alone.",
+        image: "img/8-mario.png",
+        actions: {
+            1: {
+                id: 1,
+                button: "Jump on him",
+                success: false,
+            },
+            2: {
+                id: 2,
+                button: "Tell him about your favorite Gameboy game",
+                success: true,
+                message: "He knows some of the games and wonders about certain similarities. He likes to tell you the way to your clearing.",
+                next: true,
             },
         }
     },
